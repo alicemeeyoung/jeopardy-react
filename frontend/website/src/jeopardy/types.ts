6ff877a3381daa4786064b4a80286ca9b4b95f3d
John Tran <john.tran@onehopewine.com>
2019-12-23 00:07:30 -0800

    Can switch views

--- frontend/website/src/jeopardy/types.ts
+++ frontend/website/src/jeopardy/types.ts
@@ -24,6 +24,7 @@ export const GAME_SCREEN = {
 export const TYPE_KEYS = {
   SUBTRACT_POINTS: 'SUBTRACT_POINTS',
   ADD_POINTS: 'ADD_POINTS',
+  SWITCH_VIEW: 'SWITCH_VIEW',
 } as const;
 
 type ACTIONS_TYPE = typeof TYPE_KEYS;
@@ -42,17 +43,22 @@ export type SUBTRACT_POINTS = {
   points: number;
 };
 
-export type ActionTypes = ADD_POINTS | SUBTRACT_POINTS;
+type GAME_SCREEN_TYPE = typeof GAME_SCREEN;
+type GAME_SCREEN_TYPE_KEYS = keyof GAME_SCREEN_TYPE;
+export type GAME_SCREEN_TYPE_VALUES = GAME_SCREEN_TYPE[GAME_SCREEN_TYPE_KEYS];
+
+export type SWITCH_VIEW = {
+  type: ACTIONS_TYPE['SWITCH_VIEW'];
+  view: GAME_SCREEN_TYPE_VALUES;
+};
+
+export type ActionTypes = ADD_POINTS | SUBTRACT_POINTS | SWITCH_VIEW;
 
 export type Question = {
   points: number;
   // Question
   value: string;
 };
-
-type GAME_SCREEN_TYPE = typeof GAME_SCREEN;
-type GAME_SCREEN_TYPE_KEYS = keyof GAME_SCREEN_TYPE;
-export type GAME_SCREEN_TYPE_VALUES = GAME_SCREEN_TYPE[GAME_SCREEN_TYPE_KEYS];
 
 export type StateType = {
   users: User[];
