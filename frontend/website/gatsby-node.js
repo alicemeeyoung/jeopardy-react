0d40e3f9395b03299d881a6d28516cd22318a61f
John Tran <john.tran@onehopewine.com>
2019-12-22 21:09:47 -0800

    Gatsby works with pnpm

--- frontend/website/gatsby-node.js
+++ frontend/website/gatsby-node.js
@@ -195,3 +195,16 @@ exports.onCreateWebpackConfig = ({ stage, actions }) => {
     });
   }
 };
+
+// https://github.com/pnpm/pnpm/issues/991#issuecomment-446330885
+
+let gatsbyNodeModules = require('fs').realpathSync('node_modules/gatsby');
+gatsbyNodeModules = require('path').resolve(gatsbyNodeModules, '..');
+
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [gatsbyNodeModules, 'node_modules'],
+    },
+  });
+};
