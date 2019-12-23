b3e6f69f208a54eba2ccae75eec0535e3ae713ce
Alice Kim <alicemeeyoungkim@gmail.com>
2019-12-23 00:01:52 -0800

    CSS for cell

--- frontend/website/src/jeopardy/Cell.tsx
+++ frontend/website/src/jeopardy/Cell.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  card: {
+    padding: '1rem',
+    margin: '0.2rem',
+    backgroundColor: 'blue',
+    color: 'yellow',
+  },
+});
 
 const Cell = ({ value }: { value: string }) => {
-  return <div>{value}</div>;
+  const classes = useStyles();
+  return <Card className={classes.card}>{value}</Card>;
 };
 
 export default Cell;
