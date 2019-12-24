085e06acf3b10c101b9e1ca80f789a806dd8ee35
Alice Kim <alicemeeyoungkim@gmail.com>
2019-12-23 18:53:42 -0800

    winner centering

--- src/jeopardy/pages/Scoreboard/Winner/Winner.tsx
+++ src/jeopardy/pages/Scoreboard/Winner/Winner.tsx
@@ -2,12 +2,18 @@ import * as React from 'react';
 import { useStateValue } from '../../../Redux';
 import { findWinningUsers } from './findWinningUser';
 
+const winnerStyles = {
+  fontSize: '3.5rem',
+  margin: 'auto',
+  width: '50%',
+};
+
 export function WinnerPage() {
   const [{ users }] = useStateValue();
   const winners = findWinningUsers(users);
   return (
-    <div>
-      Congratulations
+    <div css={winnerStyles}>
+      <span>Congratulations</span>
       {winners.map(winner => (
         <div key={`${winner}yop`}>{winner}</div>
       ))}
